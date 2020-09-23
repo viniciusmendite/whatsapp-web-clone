75e985ac32b298d9a356a3943fe2dd53c2bca5e9
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-22 23:50:30 -0300

    adicionando icones do material-ui, criando e estilizando area do avatar

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import './styles.css';
 
 const App = () => {
   return (
     <div className="container">
-      <div className="side-bar">...</div>
-      <div className="content-area">...</div>
+      <div className="sidebar">
+        <header>
+          <img
+            className="header--avatar"
+            src="https://www.w3schools.com/howto/img_avatar2.png"
+            alt="avatar"
+          />
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon className="stylebutton" />
+            </div>
+            <div className="header--btn">
+              <ChatIcon className="stylebutton" />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon className="stylebutton" />
+            </div>
+          </div>
+        </header>
+
+        <div className="search" />
+
+        <div className="chatlist" />
+      </div>
+      <div className="contentarea">...</div>
     </div>
   );
 };
