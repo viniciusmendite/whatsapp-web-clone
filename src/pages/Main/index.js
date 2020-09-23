1a79390a754d21a35be7cfc6c930dd10b1049f67
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-22 23:14:37 -0300

    estrutura inicial, configura eslint

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="side-bar">...</div>
+      <div className="content-area">...</div>
+    </div>
+  );
+};
+export default App;
