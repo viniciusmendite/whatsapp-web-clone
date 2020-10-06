9283fae43b15acdf4334ddf7ea5347f08fec4ea0
Vinicius Mendite <vinicius.miff@gmail.com>
2020-10-06 01:17:40 -0300

    removendo gitignore

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBh2kLiBjmYTbN-xfxVhXtAWlG8_twD9f0',
+  authDomain: 'whatsappweb-9c5e1.firebaseapp.com',
+  databaseURL: 'https://whatsappweb-9c5e1.firebaseio.com',
+  projectId: 'whatsappweb-9c5e1',
+  storageBucket: 'whatsappweb-9c5e1.appspot.com',
+  messagingSenderId: '318335213126',
+  appId: '1:318335213126:web:38651138122e34ffbd7447',
+};
+
+export default firebaseConfig;
